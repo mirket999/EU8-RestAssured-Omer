e4245638fd15938a99f821650a628b7d0e2e3a2c
omerDemirel <demirel999@yahoo.com>
2022-11-30 21:02:01 +0100

    new Json schemas added to resources directory

---
+++ src/test/java/com/cydeo/day10/JsonSchemaValidationTest.java
@@ -0,0 +1,30 @@
+package com.cydeo.day10;
+import com.cydeo.utilities.SpartanAuthTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class JsonSchemaValidationTest extends SpartanAuthTestBase {
+
+    @DisplayName("GET request to verify one spartan against to schema")
+    @Test
+    public void schemaValidation(){
+    given().
+            accept(ContentType.JSON)
+            .pathParam("id", 10)
+            .auth().basic("admin", "admin")
+    .when()
+            .get("/api/spartans/{id}")
+    .then()
+            .statusCode(200)
+            .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SingleSpartanSchema.json"))
+            .log().all();
+
+    }
+
+}
